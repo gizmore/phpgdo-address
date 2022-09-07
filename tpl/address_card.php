c9faf582780985e4fe598b7e773a570d0a4ed212
gizmore <gizmore@wechall.net>
2022-09-07 05:10:01 +0200

    '7.0.1-r1568 docs'

---
+++ tpl/address_card.php
@@ -0,0 +1,4 @@
+<?phpuse GDO\Address\GDO_Address;
+use GDO\UI\GDT_Card;
+/** @var $address GDO_Address **/
+$address instanceof GDO_Address;$card = GDT_Card::make();$card->creatorHeader();$cols = $address->gdoColumnsExcept(	'address_id', 'address_created', 'address_creator');foreach ($cols as $gdt){	if ($gdt->getVar())	{		$card->addField($gdt);	}}echo $card->render();
--- tpl/card/address.php
+++
@@ -1,4 +0,0 @@
-<?phpuse GDO\Address\GDO_Address;
-use GDO\UI\GDT_Card;
-/** @var $address GDO_Address **/
-$address instanceof GDO_Address;$card = GDT_Card::make();foreach ($address->gdoColumnsExcept('address_id', 'address_created', 'address_creator') as $gdt){	if ($gdt->getVar())	{		$card->addField($gdt);	}}echo $card->renderCard();
